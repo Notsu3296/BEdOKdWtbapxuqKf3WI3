f6b9b1ea69e6af03f92119afc9e3c30647996134
Notsu3296 <165113704+Notsu3296@users.noreply.github.com>
2026-05-20 10:35:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,12 @@
 const target = document.querySelector("#target");
-const trackedModel = document.querySelector("#trackedModel");
-const fixedModel = document.querySelector("#fixedModel");
-
-let isFixed = false;
+const model = document.querySelector("#model");
 
 target.addEventListener("targetFound", () => {
   console.log("Marker Found");
+  model.setAttribute("visible", true);
+});
 
-  if (isFixed) return;
-
-  trackedModel.setAttribute("visible", true);
-
-  const trackedObject = trackedModel.object3D;
-  const fixedObject = fixedModel.object3D;
-
-  trackedObject.updateMatrixWorld(true);
-
-  trackedObject.getWorldPosition(fixedObject.position);
-  trackedObject.getWorldQuaternion(fixedObject.quaternion);
-  trackedObject.getWorldScale(fixedObject.scale);
-
-  fixedModel.setAttribute("visible", true);
-  trackedModel.setAttribute("visible", false);
-
-  isFixed = true;
-
-  console.log("Model Fixed");
+target.addEventListener("targetLost", () => {
+  console.log("Marker Lost");
+  model.setAttribute("visible", true);
 });
