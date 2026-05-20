f7b1d30e29f4357e9dedbc95e18adf4e54953443
Notsu3296 <165113704+Notsu3296@users.noreply.github.com>
2026-05-20 10:30:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("JavaScript 接続OK")
+const target = document.querySelector("#target");
+const model = document.querySelector("#model");
+
+let hasFound = false;
+
+target.addEventListener("targetFound", () => {
+    console.log("Marker Found");
+
+    hasFound = true;
+
+    model.setAttribute("visible", true);
+});
+
+target.addEventListener("targetLost", () => {
+    console.log("Marker Lost");
+
+    if (hasFound) {
+        model.setAttribute("visible", true);
+    }
+});
