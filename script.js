b6e01fd7c8cdedb37b680c245dc9c29f000982a5
Notsu3296 <165113704+Notsu3296@users.noreply.github.com>
2026-05-20 11:20:10 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+<script type="module" src="script.js"></script>
+
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.165.0/build/three.module.js";
 import { ARButton } from "https://cdn.jsdelivr.net/npm/three@0.165.0/examples/jsm/webxr/ARButton.js";
 import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.165.0/examples/jsm/loaders/GLTFLoader.js";
