c17e104cdc0259fc1dceca0a60259312216f335a
Notsu3296 <165113704+Notsu3296@users.noreply.github.com>
2026-05-20 13:14:25 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -156,8 +156,7 @@ function onSelect() {
   placedModel.quaternion.copy(quaternion);
 
   // モデル自体の向き補正
-  placedModel.rotateX(Math.PI / 2);
-  PlacedModel.rotateY(Math.PI / 2);
+  placedModel.rotateX(Math.PI );
 
   placedModel.scale.set(MODEL_SCALE, MODEL_SCALE, MODEL_SCALE);
   placedModel.visible = true;
